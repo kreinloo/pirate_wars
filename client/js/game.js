18406ed3b385ff20b9ed8654feacd87062b43a41
Kristjan Reinloo <kristjanreinloo@gmail.com>
2012-03-13 17:16:04 +0200

    It is now possible to rotate the ships.

--- client/js/game.js
+++ client/js/game.js
@@ -17,187 +17,244 @@ function Client () {
 			gameTable[i][j] = 0;
 	}
 
-	return {
 
-		getID : function () { return id; },
 
-		getName : function () { return name; },
+	this.getID = function () { return id; }
 
-		getGameTable : function () { return gameTable; },
+	this.getName = function () { return name; }
 
-		printGameTable : function () {
-			for (var i = 0; i < 10; i++)
-				console.log (gameTable[i]);
-		},
+	this.getGameTable = function () { return gameTable; }
 
-		addShip : function (cellId, ship) {
-			
-			var coords = cellId.split("_");
-			var row = parseInt(coords[1]);
-			var col = parseInt(coords[2]);
-			var length = parseInt($(ship.draggable).data("obj").length);
-			var direction = $(ship.draggable).data("obj").direction;
+	this.printGameTable = function () {
+		for (var i = 0; i < 10; i++)
+			console.log (gameTable[i]);
+	}
 
-			console.log("row: " + row + " col: " + col + " length: " + length + " direction: " + direction);
+	this.addShip = function (cellId, ship) {
 
-			switch (length) {
+		var coords = cellId.split("_");
+		var row = parseInt(coords[1]);
+		var col = parseInt(coords[2]);
 
-			case 1:
-				if (gameTable[row][col] == 0) {
+		var length = ship.length;
+		var direction = ship.direction;
+
+		console.log("row: " + row + " col: " + col + " length: " + length + " direction: " + direction);
+
+		switch (length) {
+
+		case 1:
+			if (gameTable[row][col] == 0) {
+				gameTable[row][col] = 1;
+				this.printGameTable();
+				ship.cellId = cellId;
+				ship.coords = [ [row, col] ];
+				return true;
+			}
+			else {
+				this.printGameTable();
+				return false;
+			}
+			break;
+
+		case 2:
+
+			if (direction == "horizontal") {
+				if (gameTable[row][col] == 0 && col+1 <= 9 && gameTable[row][col+1] == 0) {
 					gameTable[row][col] = 1;
+					gameTable[row][col+1] = 1;
 					this.printGameTable();
-					return [ [row, col] ];
+					ship.cellId = cellId;
+					ship.coords = [ [row, col], [row, col+1] ];
+					return true;
 				}
 				else {
 					this.printGameTable();
-					return [];
+					return false;
 				}
-				break;
+			}
 
-			case 2:
-
-				if (direction == "horizontal") {
-					if (gameTable[row][col] == 0 && col+1 <= 9 && gameTable[row][col+1] == 0) {
-						gameTable[row][col] = 1;
-						gameTable[row][col+1] = 1;
-						this.printGameTable();
-						return [ [row, col], [row, col+1] ];
-					}
-					else {
-						this.printGameTable();
-						return [];
-					}
+			else {
+				if (gameTable[row][col] == 0 && row+1 <= 9 && gameTable[row+1][col] == 0) {
+					gameTable[row][col] = 1;
+					gameTable[row+1][col] = 1;
+					this.printGameTable();
+					ship.cellId = cellId;
+					ship.coords = [ [row, col], [row+1, col] ];
+					return true;
 				}
-
 				else {
-					if (gameTable[row][col] == 0 && row+1 <= 9 && gameTable[row+1][col] == 0) {
-						gameTable[row][col] = 1;
-						gameTable[row+1][col] = 1;
-						this.printGameTable();
-						return [ [row, col], [row+1, col] ];
-					}
-					else {
-						this.printGameTable();
-						return [];
-					}
+					this.printGameTable();
+					return false;
 				}
-				break;
+			}
+			break;
 
-			case 3:
+		case 3:
 
-				if (direction == "horizontal") {
-					if (gameTable[row][col] == 0 && col+1 <= 9 && gameTable[row][col+1] == 0 &&
-						col-1 >= 0 && gameTable[row][col-1] == 0) {
+			if (direction == "horizontal") {
+				if (gameTable[row][col] == 0 && col+1 <= 9 && gameTable[row][col+1] == 0 &&
+					col-1 >= 0 && gameTable[row][col-1] == 0) {
 
-						gameTable[row][col] = 1;
-						gameTable[row][col+1] = 1;
-						gameTable[row][col-1] = 1;
-						this.printGameTable();
-						return [ [row, col], [row, col+1], [row, col-1] ];
-					}
-					else {
-						this.printGameTable();
-						return [];
-					}
+					gameTable[row][col] = 1;
+					gameTable[row][col+1] = 1;
+					gameTable[row][col-1] = 1;
+
+					this.printGameTable();
+					ship.cellId = cellId;
+					ship.coords = [ [row, col], [row, col+1], [row, col-1] ];
+					return true;
 				}
-
 				else {
-					if (gameTable[row][col] == 0 && row+1 <= 9 && gameTable[row+1][col] == 0 &&
-						row-1 >= 0 && gameTable[row-1][col] == 0) {
-
-						gameTable[row][col] = 1;
-						gameTable[row+1][col] = 1;
-						gameTable[row-1][col] = 1;
-						this.printGameTable();
-						return [ [row, col], [row+1, col], [row-1, col] ];
-					}
-					else {
-						this.printGameTable();
-						return [];
-					}
+					this.printGameTable();
+					return false;
 				}
+			}
 
-				break;
+			else {
+				if (gameTable[row][col] == 0 && row+1 <= 9 && gameTable[row+1][col] == 0 &&
+					row-1 >= 0 && gameTable[row-1][col] == 0) {
 
-			case 4:
+					gameTable[row][col] = 1;
+					gameTable[row+1][col] = 1;
+					gameTable[row-1][col] = 1;
 
-				if (direction == "horizontal") {
-					if (gameTable[row][col] == 0 &&
-						col+1 <= 9 && gameTable[row][col+1] == 0 &&
-						col+2 <= 9 && gameTable[row][col+2] == 0 &&
-						col-1 >= 0 && gameTable[row][col-1] == 0) {
-
-						gameTable[row][col] = 1;
-						gameTable[row][col+1] = 1;
-						gameTable[row][col+2] = 1;
-						gameTable[row][col-1] = 1;
-
-						this.printGameTable();
-						return [ [row, col], [row, col+1], [row, col+2], [row, col-1] ];
-					}
-					else {
-						this.printGameTable();
-						return [];
-					}
+					this.printGameTable();
+					ship.cellId = cellId;
+					ship.coords = [ [row, col], [row+1, col], [row-1, col] ];
+					return true;
 				}
-
 				else {
-					if (gameTable[row][col] == 0 &&
-						row+1 <= 9 && gameTable[row+1][col] == 0 &&
-						row+2 <= 9 && gameTable[row+2][col] == 0 &&
-						row-1 >= 0 && gameTable[row-1][col] == 0) {
-
-						gameTable[row][col] = 1;
-						gameTable[row+1][col] = 1;
-						gameTable[row+2][col] = 1;
-						gameTable[row-1][col] = 1;
-
-						this.printGameTable();
-						return [ [row, col], [row+1, col], [row+2, col], [row-1, col] ];
-					}
-					else {
-						this.printGameTable();
-						return [];
-					}
-
+					this.printGameTable();
+					return false;
 				}
+			}
 
-				break;
+			break;
+
+		case 4:
+
+			if (direction == "horizontal") {
+				if (gameTable[row][col] == 0 &&
+					col+1 <= 9 && gameTable[row][col+1] == 0 &&
+					col+2 <= 9 && gameTable[row][col+2] == 0 &&
+					col-1 >= 0 && gameTable[row][col-1] == 0) {
+
+					gameTable[row][col] = 1;
+					gameTable[row][col+1] = 1;
+					gameTable[row][col+2] = 1;
+					gameTable[row][col-1] = 1;
+
+					this.printGameTable();
+					ship.cellId = cellId;
+					ship.coords = [ [row, col], [row, col+1], [row, col+2], [row, col-1] ];
+					return true;
+				}
+				else {
+					this.printGameTable();
+					return false;
+				}
+			}
+
+			else {
+				if (gameTable[row][col] == 0 &&
+					row+1 <= 9 && gameTable[row+1][col] == 0 &&
+					row+2 <= 9 && gameTable[row+2][col] == 0 &&
+					row-1 >= 0 && gameTable[row-1][col] == 0) {
+
+					gameTable[row][col] = 1;
+					gameTable[row+1][col] = 1;
+					gameTable[row+2][col] = 1;
+					gameTable[row-1][col] = 1;
+
+					this.printGameTable();
+					ship.cellId = cellId;
+					ship.coords = [ [row, col], [row+1, col], [row+2, col], [row-1, col] ];
+					return true;
+				}
+				else {
+					this.printGameTable();
+					return false;
+				}
 
 			}
 
-		},
-
-		deleteShip : function (coords) {
-
-			console.log("deleting coords: ");
-			console.log(coords);
-			for (var coord in coords) {
-				var c = coords[coord];
-				gameTable[c[0]][c[1]] = 0;
-			}
-
-		},
-
-		rotateShip : function (cellId, ship) {
-
-			console.log(cellId);
+			break;
 
 		}
+
 	}
+
+	this.deleteShip = function (coords) {
+		for (var coord in coords) {
+			var c = coords[coord];
+			gameTable[c[0]][c[1]] = 0;
+		}
+	}
+
+	this.rotateShip = function (ship) {
+
+		if ( !$(ship).data("obj").cellId ) {
+			$(ship).data("obj").flipDirection();
+			return true;
+		}
+
+		var tmpCell = $(ship).data("obj").cellId;
+
+		this.deleteShip( $(ship).data("obj").coords );
+
+		if ( $(ship).data("obj").direction == "horizontal" ) {
+			$(ship).data("obj").direction = "vertical";
+
+			if ( $(ship).data("obj").length >= 3 ) {
+				var coords = tmpCell.split("_");
+				if ( parseInt(coords[1]) <= 8 && parseInt(coords[2]) >= 1 ) {
+					var row = parseInt(coords[1]) + 1;
+					var col = parseInt(coords[2]) - 1;
+					tmpCell = coords[0] + "_" + row + "_" + col;
+					console.log(tmpCell);
+				}
+			}
+		}
+		else {
+			$(ship).data("obj").direction = "horizontal";
+
+			if ( $(ship).data("obj").length >= 3 ) {
+				var coords = tmpCell.split("_");
+				if ( parseInt(coords[1]) >= 1 && parseInt(coords[2]) <= 8 ) {
+					var row = parseInt(coords[1]) - 1;
+					var col = parseInt(coords[2]) + 1;
+					tmpCell = coords[0] + "_" + row + "_" + col;
+					console.log(tmpCell);
+				}
+			}
+
+		}
+
+		var res = this.addShip(tmpCell, $(ship).data("obj"));
+		if (res) {
+			return true;
+		}
+		else {
+			$(ship).data("obj").flipDirection();
+			this.addShip( $(ship).data("obj").cellId, $(ship).data("obj"));
+			return false;
+		}
+	}
+
 
 };
 
 var SHIPS = {
 
-	SHIP_1_horizontal : "gfx/SHIP_1.png",
-	SHIP_1_vertical : "gfx/SHIP_1.png",
+	SHIP_1_horizontal : "gfx/SHIP_1_horizontal.png",
+	SHIP_1_vertical : "gfx/SHIP_1_vertical.png",
 
-	SHIP_2_horizontal : "gfx/SHIP_2.png",
-	SHIP_2_vertical : "gfx/SHIP_2.png",
+	SHIP_2_horizontal : "gfx/SHIP_2_horizontal.png",
+	SHIP_2_vertical : "gfx/SHIP_2_vertical.png",
 
-	SHIP_3_horizontal : "gfx/SHIP_3.png",
-	SHIP_3_vertical : "gfx/SHIP_3.png",
+	SHIP_3_horizontal : "gfx/SHIP_3_horizontal.png",
+	SHIP_3_vertical : "gfx/SHIP_3_vertical.png",
 
 	SHIP_4_horizontal : "gfx/SHIP_4_horizontal.png",
 	SHIP_4_vertical : "gfx/SHIP_4_vertical.png"
@@ -213,72 +270,31 @@ $(document).ready(function () {
 
 	$("#game-table-ships").css("display", "inline");
 
-/*
-	$("#game-table-ships").append(
-		$("<img>").attr("src", SHIPS.SHIP_1_horizontal).
-				data("ship-length", 1).
-				data("direction", "horizontal").
-				draggable({
-					snap : ".game-table-cell",
-					snapMode: "inner",
-					containment: "#game-tablearea" })
-	);
-
-	$("#game-table-ships").append(
-	$("<img>").attr("src", SHIPS.SHIP_2_horizontal).
-				data("ship-length", 2).
-				data("direction", "horizontal").
-				draggable({
-					snap : ".game-table-cell",
-					snapMode: "inner",
-					containment: "#game-tablearea" })
-	);
-
-	$("#game-table-ships").append(
-	$("<img>").attr("src", SHIPS.SHIP_3_horizontal).
-				data("ship-length", 3).
-				data("direction", "horizontal").
-				draggable({
-					snap : ".game-table-cell",
-					snapMode: "inner",
-					containment: "#game-tablearea" })
-	);
-
-	$("#game-table-ships").append(
-	$("<img>").attr("src", SHIPS.SHIP_4_horizontal).
-				data("ship-length", 4).
-				data("direction", "horizontal").
-				draggable({
-					snap : ".game-table-cell",
-					snapMode: "inner",
-					containment: "#game-tablearea",
-
-					start : function (event, ui) {
-
-						$(this).data("top", $(this).css("top"));
-						$(this).data("left", $(this).css("left"));
-					
-					}})
-	);
-*/
-
 	$(".game-table-cell").droppable({
 		drop : function (event, ui) {
-			var result = client.addShip( $(this).attr("id"), ui );
-			if (result.length == 0) {
+			var result = client.addShip( $(this).attr("id"), ui.draggable.data("obj") );
+			if (!result) {
 				$(ui.draggable).css("top", $(ui.draggable).data("top"));
 				$(ui.draggable).css("left", $(ui.draggable).data("left"));
 			}
 			else {
-				$(ui.draggable).data("obj").saveCoordinates(result);
 			}
 		}
 
-	})
+	});
 
 	ships.push( new Ship (1, "horizontal", client) );
+	ships.push( new Ship (1, "horizontal", client) );
+	ships.push( new Ship (1, "vertical", client) );
+	ships.push( new Ship (1, "vertical", client) );
+
 	ships.push( new Ship (2, "horizontal", client) );
+	ships.push( new Ship (2, "horizontal", client) );
+	ships.push( new Ship (2, "vertical", client) );
+
 	ships.push( new Ship (3, "horizontal", client) );
+	ships.push( new Ship (3, "vertical", client) );
+
 	ships.push( new Ship (4, "horizontal", client) );
 });
 
@@ -289,7 +305,7 @@ function Ship (length, direction, client) {
 	this.direction = direction;
 	this.client = client;
 	this.coords = [];
-
+	this.cellId = null;
 	this.element = $("<img>");
 
 	switch (this.length) {
@@ -328,7 +344,6 @@ function Ship (length, direction, client) {
 		snap : ".game-table-cell",
 		snapMode: "inner",
 		containment: "#game-tablearea",
-
 		start : function (event, ui) {
 			$(this).data("top", $(this).css("top"));
 			$(this).data("left", $(this).css("left"));
@@ -336,17 +351,71 @@ function Ship (length, direction, client) {
 				$(this).data("obj").client.deleteShip( $(this).data("obj").coords );
 				$(this).data("obj").deleteCoords();
 			}
-	}});
+		}
+	});
+
+	this.element.mousedown(function (event) {
+		if (event.which == 2) {
+			$(this).data("obj").rotateShip();
+		}
+	});
 
 	this.element.data("obj", this);
+	this.element.css("position", "absolute");
 
 	$("#game-table-ships").append(this.element);
 
-	this.saveCoordinates = function (coords) {
-		this.coords = coords;
+	this.saveCoordinates = function (coords) { this.coords = coords; };
+
+	this.deleteCoords = function () {
+		this.coords = [];
+		this.cellId = null;
 	};
 
-	this.deleteCoords = function () { this.coords = []; };
+	this.rotateShip = function () {
+
+		var res = this.client.rotateShip(this.element);
+
+		if (res) {
+			switch (this.length) {
+
+			case 1:
+				if (this.direction == "horizontal")
+					this.element.attr("src", SHIPS.SHIP_1_horizontal);
+				else
+					this.element.attr("src", SHIPS.SHIP_1_vertical);
+				break;
+
+			case 2:
+				if (this.direction == "horizontal")
+					this.element.attr("src", SHIPS.SHIP_2_horizontal);
+				else
+					this.element.attr("src", SHIPS.SHIP_2_vertical);
+				break;
+
+			case 3:
+				if (this.direction == "horizontal")
+					this.element.attr("src", SHIPS.SHIP_3_horizontal);
+				else
+					this.element.attr("src", SHIPS.SHIP_3_vertical);
+				break;
+
+			case 4:
+				if (this.direction == "horizontal")
+					this.element.attr("src", SHIPS.SHIP_4_horizontal);
+				else
+					this.element.attr("src", SHIPS.SHIP_4_vertical);
+				break;
+			}
+		}
+	}
+
+	this.flipDirection = function () {
+		if (this.direction == "horizontal")
+			this.direction = "vertical";
+		else
+			this.direction = "horizontal";
+	}
 
 }
 
