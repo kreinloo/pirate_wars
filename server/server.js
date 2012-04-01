d0f95fc5bc70fb02e59dbe19fd87a264fc5a1437
Kristjan Reinloo <kristjanreinloo@gmail.com>
2012-04-01 17:02:23 +0300

    Renamed some files and variables.

--- server/server.js
+++ server/server.js
@@ -8,13 +8,12 @@
 var io = require("socket.io").listen(8001);
 var node_static = require("node-static");
 var http = require("http");
-var util = require("util");
 
 // our gameserver
 var pw_server = require("pw_server");
 
 pw_server.setIO(io);
-pw_server.bindListeners(io);
+pw_server.bindListeners();
 
 // static file server
 var file = new (node_static.Server)("../client");
