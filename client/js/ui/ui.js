87dec47d3696371f380c737995c0e5c3d16b589e
Kristjan Reinloo <kristjanreinloo@gmail.com>
2012-05-08 21:37:07 +0300

    Minor improvements.

--- client/js/ui/ui.js
+++ client/js/ui/ui.js
@@ -323,5 +323,36 @@ var ui = new UI();
 $(document).ready(function () {
 	ui.loadFooter();
 	ui.load("login");
-
 });
+
+/*
+	Moved FB login code here, I did not create a new js file
+	in order to avoid the extra GET request.
+*/
+window.fbAsyncInit = function() {
+	FB.init({
+		appId      : '304040883005114', // App ID
+		channelUrl : '//piraadid.ahju.eu/channel.html', // Channel File
+		status     : true, // check login status
+		cookie     : true, // enable cookies to allow the server to access the session
+		xfbml      : true  // parse XFBML
+	});
+function login (response) {
+	if (response.status === "connected") {
+		Client.connect({
+			id : FB.getUserID(),
+			token : FB.getAccessToken()
+		});
+	}
+}
+FB.Event.subscribe("auth.statusChange", login);
+};
+
+// Load the SDK Asynchronously
+(function(d){
+	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+	if (d.getElementById(id)) {return;}
+	js = d.createElement('script'); js.id = id; js.async = true;
+	js.src = "//connect.facebook.net/en_US/all.js";
+	ref.parentNode.insertBefore(js, ref);
+}(document));
